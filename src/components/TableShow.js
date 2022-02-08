528214a0b8e3e5d6b9dc3bcde31aa6532ddb2680
ted10lasso <ted10lasso@gmail.com>
2022-02-08 11:18:58 +0530

    #2 styles

--- src/components/TableShow.js
+++ src/components/TableShow.js
@@ -1,14 +1,13 @@
-import React, { useContext, useState } from 'react';
-import { Button, Modal, ModalBody, Table } from 'react-bootstrap';
-import { withRouter } from 'react-router-dom';
-import ContextData from '../context/context';
+import React, { useContext, useState } from "react"
+import { Button, Modal, ModalBody, Table } from "react-bootstrap"
+import { withRouter } from "react-router-dom"
+import ContextData from "../context/context"
 
 function TableShow(props) {
   const contextValue = useContext(ContextData)
-  console.log(contextValue.storeData);
-  console.log(contextValue);
- // const [show, setShow] = useState(false)
-
+  console.log(contextValue.storeData)
+  console.log(contextValue)
+  // const [show, setShow] = useState(false)
 
   const deleteData = (index) => {
     const array = [...contextValue.storeData]
@@ -17,48 +16,66 @@ function TableShow(props) {
   }
 
   const editData = (data, index) => {
-    console.log(contextValue.showEditModal);
+    console.log(contextValue.showEditModal)
     contextValue.setEditModal(true)
-   // setShow(true)
-    
-    console.log('its commign');
+    // setShow(true)
+
+    console.log("its commign")
     // const array=[...contextValue.storeData]
-    contextValue.setSelectedEmployee(data);
+    contextValue.setSelectedEmployee(data)
     // array.splice()
     contextValue.setIndex(index)
-props.history.push('/editEmployee')
-    
+    props.history.push("/editEmployee")
   }
-  return <div>
-  
-    <Table striped bordered hover variant="dark">
-      <thead>
-        <tr>
-          <th>Full Name</th>
-          <th>Designation</th>
-          <th>Salary</th>
-          <th>Age</th>
-          <th>Edit</th>
-          <th>Delete</th>
-
-        </tr>
-      </thead>
-      <tbody>
-        {contextValue.storeData && contextValue.storeData.map((data, index) => {
-          return (
-            <tr key={index}>
-              <td>{data.fullName}</td>
-              <td>{data.designation}</td>
-              <td>{data.salary}</td>
-              <td>{data.age}</td>
-              <td><button onClick={() => { editData(data, index) }}>Edit</button></td>
-              <td><button onClick={(index) => { deleteData(index) }}>Delete</button></td>
-            </tr>
-          )
-        })}
-      </tbody>
-    </Table>
-  </div>;
+  return (
+    <div>
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>Full Name</th>
+            <th>Designation</th>
+            <th>Salary</th>
+            <th>Age</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contextValue.storeData &&
+            contextValue.storeData.map((data, index) => {
+              return (
+                <tr key={index}>
+                  <td>{data.fullName}</td>
+                  <td>{data.designation}</td>
+                  <td>{data.salary}</td>
+                  <td>{data.age}</td>
+                  <td>
+                    <button
+                      className="btn btn-warning"
+                      onClick={() => {
+                        editData(data, index)
+                      }}
+                    >
+                      Edit
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      className="btn btn-danger"
+                      onClick={(index) => {
+                        deleteData(index)
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              )
+            })}
+        </tbody>
+      </Table>
+    </div>
+  )
 }
 
-export default withRouter(TableShow);
+export default withRouter(TableShow)
